eaf9e99f56db9b542194b8dc85fe37b34157c284
Махнатеев Степан <stepan01062004@mail.ru.>
2025-09-22 20:56:29 +0300

    Fix migrations

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,13 +1,53 @@
-DROP TABLE IF EXIST regions;
+CREATE TABLE regions (
+    region_id SERIAL PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
+    geom GEOMETRY NOT NULL
+);
 
-DROP TABLE IF EXIST cities;
+CREATE TABLE cities (
+    city_id SERIAL PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
+    region_id INTEGER,
+    geom GEOMETRY NOT NULL,
+    CONSTRAINT fk_region FOREIGN KEY (region_id) REFERENCES regions(region_id)
+);
 
-DROP TABLE IF EXIST districts;
+CREATE TABLE districts (
+    district_id SERIAL PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
+    city_id INTEGER,
+    geom GEOMETRY NOT NULL,
+    CONSTRAINT fk_city FOREIGN KEY (city_id) REFERENCES cities(city_id)
+);
 
-DROP TABLE IF EXIST types_marks;
+CREATE TABLE types_marks (
+    type_mark_id SERIAL PRIMARY KEY,
+    name VARCHAR(40) NOT NULL
+);
 
-DROP TABLE IF EXIST users;
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
+    rating INTEGER
+);
 
-DROP TABLE IF EXIST marks;
+CREATE TABLE marks (
+    mark_id SERIAL PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
+    geom GEOMETRY NOT NULL,
+    type_mark_id INTEGER,
+    user_id INTEGER,
+    district_id INTEGER,
+    number_votes INTEGER,
+    number_checks INTEGER,
+    CONSTRAINT fk_type_mark FOREIGN KEY (type_mark_id) REFERENCES types_marks(type_mark_id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(user_id),
+    CONSTRAINT fk_district FOREIGN KEY (district_id) REFERENCES districts(district_id)
+);
 
-DROP TABLE IF EXIST tasks;
+CREATE TABLE tasks (
+    task_id SERIAL PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
+    user_id INTEGER,
+    CONSTRAINT fk_user_task FOREIGN KEY (task_id) REFERENCES users(user_id)
+);
