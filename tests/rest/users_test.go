1ed2b6389120d337fbf8c515a530a9dde83798bc
Махнатеев Степан <stepan01062004@mail.ru.>
2025-10-20 19:06:25 +0300

    Add basic functional tests for users endpoints

--- tests/rest/users_test.go
+++ tests/rest/users_test.go
@@ -1,12 +1,15 @@
-//go:build functional
+//go:build functional && rest
 
 package tests
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"testing"
 
+	usersrest "github.com/PritOriginal/problem-map-server/internal/handler/users"
+	"github.com/PritOriginal/problem-map-server/pkg/responses"
 	"github.com/PritOriginal/problem-map-server/tests/rest/suite"
 	"github.com/stretchr/testify/require"
 )
@@ -19,5 +22,28 @@ func TestGetUsers(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, http.StatusOK, resp.StatusCode)
 
-	resp.Body.Close()
+	defer resp.Body.Close()
+
+	var response responses.SucceededResponse[usersrest.GetUsersResponse]
+
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	require.NoError(t, err)
+	require.NotNil(t, response.Payload.Users)
+}
+
+func TestGetUserById(t *testing.T) {
+	st := suite.New(t)
+
+	resp, err := http.Get(fmt.Sprintf("http://%s:%d/users/1", st.Cfg.REST.Host, st.Cfg.REST.Port))
+
+	require.NoError(t, err)
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+
+	defer resp.Body.Close()
+
+	var response responses.SucceededResponse[usersrest.GetUserByIdResponse]
+
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	require.NoError(t, err)
+	require.NotNil(t, response.Payload.User)
 }
