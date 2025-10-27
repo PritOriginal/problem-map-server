32c4005094d113cfe4fee6c154e98da6a774e747
Махнатеев Степан <stepan01062004@mail.ru.>
2025-10-27 22:28:12 +0300

    Add functional test to add a mark

--- tests/rest/map_test.go
+++ tests/rest/map_test.go
@@ -3,15 +3,22 @@
 package tests
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"testing"
 
+	authrest "github.com/PritOriginal/problem-map-server/internal/handler/auth"
 	maprest "github.com/PritOriginal/problem-map-server/internal/handler/map"
+	"github.com/PritOriginal/problem-map-server/internal/models"
 	"github.com/PritOriginal/problem-map-server/pkg/responses"
 	"github.com/PritOriginal/problem-map-server/tests/rest/suite"
+	"github.com/brianvoe/gofakeit/v7"
 	"github.com/stretchr/testify/require"
+	"github.com/twpayne/go-geom"
 )
 
 func TestGetRegions(t *testing.T) {
@@ -83,5 +90,45 @@ func TestGetMarks(t *testing.T) {
 }
 
 func TestAddMark(t *testing.T) {
-	//TODO
+	st := suite.New(t)
+
+	signInResponse := signIn(t, st, authrest.SignInRequest{
+		Username: "user4",
+		Password: "1234qwer",
+	})
+
+	mark := models.Mark{
+		Geom:         models.NewPoint(geom.Coord{41.497976, 52.707605}),
+		TypeMarkID:   1,
+		MarkStatusID: 1,
+		UserID:       1,
+		DistrictID:   1,
+	}
+
+	jsonData, err := json.Marshal(mark)
+	require.NoError(t, err)
+
+	b := &bytes.Buffer{}
+	w := multipart.NewWriter(b)
+
+	w.WriteField("data", string(jsonData))
+
+	image := gofakeit.ImageJpeg(10, 10)
+	fw, err := w.CreateFormFile("photo", "test.jpg")
+	io.Copy(fw, bytes.NewBuffer(image))
+
+	w.Close()
+
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("http://%s:%d/map/marks", st.Cfg.REST.Host, st.Cfg.REST.Port), b)
+	require.NoError(t, err)
+
+	req.Header.Set("Authorization", "Bearer "+signInResponse.Payload.AccessToken)
+	req.Header.Set("Content-Type", w.FormDataContentType())
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusCreated, resp.StatusCode)
+
+	defer resp.Body.Close()
+
 }
