e75b784c5c883ecc78be37ccb96a34602da371ce
Махнатеев Степан <stepan01062004@mail.ru.>
2025-12-12 20:41:12 +0300

    Update PhotosRepository in map use case

--- internal/usecase/map.go
+++ internal/usecase/map.go
@@ -21,8 +21,9 @@ type MapRepository interface {
 }
 
 type PhotosRepository interface {
-	AddPhotos(photos [][]byte) error
-	GetPhotos() error
+	AddPhotos(ctx context.Context, markId, reviewId int, photos [][]byte) error
+	GetPhotos(ctx context.Context) (map[int]map[int][]string, error)
+	GetPhotosByMarkId(ctx context.Context, markId int) (map[int]map[int][]string, error)
 }
 
 type Map struct {
