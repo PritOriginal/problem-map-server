d0e72b2197ce7cdfa3af59b64e3620581e2b9729
Махнатеев Степан <stepan01062004@mail.ru.>
2026-01-04 13:59:03 +0300

    Add set and get methods to redis storage

--- internal/storage/redis/redis.go
+++ internal/storage/redis/redis.go
@@ -2,7 +2,9 @@ package redis
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/PritOriginal/problem-map-server/internal/config"
 	"github.com/redis/go-redis/v9"
@@ -34,4 +36,26 @@ func (r *Redis) Stop() error {
 
 func (r *Redis) Exists(ctx context.Context, key string) bool {
 	return r.Client.Exists(ctx, key).Val() > 0
+}
+
+func (r *Redis) Get(ctx context.Context, key string, v any) error {
+	data, err := r.GetBytes(ctx, key)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(data, v); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *Redis) GetBytes(ctx context.Context, key string) ([]byte, error) {
+	return r.Client.Get(ctx, key).Bytes()
+}
+
+func (r *Redis) Set(ctx context.Context, key string, value any, expiration time.Duration) error {
+	if err := r.Client.Set(ctx, key, value, expiration).Err(); err != nil {
+		return err
+	}
+	return nil
 }
