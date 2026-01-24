f18be7d3ea55b737a0435c579e6b59341f5d26da
Махнатеев Степан <stepan01062004@mail.ru.>
2026-01-24 18:48:39 +0300

    Update names arguments in auth usecase methods

--- internal/usecase/auth_test.go
+++ internal/usecase/auth_test.go
@@ -107,7 +107,7 @@ func (suite *AuthSuite) TestSignIn() {
 
 	for _, tt := range tests {
 		suite.Run(tt.name, func() {
-			usersRepoCall := suite.usersRepo.On("GetUserByUsername", mock.Anything, mock.AnythingOfType("string")).Once()
+			usersRepoCall := suite.usersRepo.On("GetUserByLogin", mock.Anything, mock.AnythingOfType("string")).Once()
 			if !tt.wantErr {
 				usersRepoCall.Return(models.User{
 					PasswordHash: passwordHash,
@@ -116,7 +116,7 @@ func (suite *AuthSuite) TestSignIn() {
 				usersRepoCall.Return(models.User{}, errors.New(""))
 			}
 
-			_, _, gotErr := suite.uc.SignIn(context.Background(), "username", "password")
+			_, _, gotErr := suite.uc.SignIn(context.Background(), "login", "password")
 
 			if !tt.wantErr {
 				suite.NoError(gotErr)
