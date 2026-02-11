bb0550d70b32a34b1537eee774f9810f41b0db24
Махнатеев Степан <stepan01062004@mail.ru.>
2026-02-11 13:49:55 +0300

    Update checks use case tests - Add init MockMarkStatusUpdater - Add tests for MarkStatusUpdater

--- internal/usecase/checks_test.go
+++ internal/usecase/checks_test.go
@@ -18,6 +18,7 @@ type ChecksSuite struct {
 	suite.Suite
 	uc         *usecase.Checks
 	log        *slog.Logger
+	updater    *usecase.MockMarkStatusUpdater
 	marksRepo  *usecase.MockMarksRepository
 	checksRepo *usecase.MockChecksRepository
 	photosRepo *usecase.MockPhotosRepository
@@ -25,10 +26,15 @@ type ChecksSuite struct {
 
 func (suite *ChecksSuite) SetupSuite() {
 	suite.log = slogdiscard.NewDiscardLogger()
+	suite.updater = usecase.NewMockMarkStatusUpdater(suite.T())
 	suite.marksRepo = usecase.NewMockMarksRepository(suite.T())
 	suite.checksRepo = usecase.NewMockChecksRepository(suite.T())
 	suite.photosRepo = usecase.NewMockPhotosRepository(suite.T())
-	suite.uc = usecase.NewChecks(suite.log, suite.marksRepo, suite.checksRepo, suite.photosRepo)
+	suite.uc = usecase.NewChecks(suite.log, suite.updater, usecase.ChecksRepositories{
+		Marks:  suite.marksRepo,
+		Checks: suite.checksRepo,
+		Photos: suite.photosRepo,
+	})
 }
 
 func TestChecks(t *testing.T) {
@@ -36,46 +42,95 @@ func TestChecks(t *testing.T) {
 }
 
 func (suite *ChecksSuite) TestAddCheck() {
+	type method[T any] struct {
+		data T
+		err  error
+	}
+
 	tests := []struct {
-		name             string
-		addCheckWantErr  bool
-		addPhotosWantErr bool
+		name      string
+		addCheck  method[int64]
+		addPhotos method[any]
+		update    method[any]
 	}{
 		{
-			name:             "Ok",
-			addCheckWantErr:  false,
-			addPhotosWantErr: false,
+			name: "Ok",
+			addCheck: method[int64]{
+				data: int64(1),
+				err:  nil,
+			},
+			addPhotos: method[any]{
+				err: nil,
+			},
+			update: method[any]{
+				err: nil,
+			},
 		},
 		{
-			name:             "ErrAddCheck",
-			addCheckWantErr:  true,
-			addPhotosWantErr: false,
+			name: "ErrAddCheck",
+			addCheck: method[int64]{
+				data: int64(0),
+				err:  errors.New(""),
+			},
+			addPhotos: method[any]{
+				err: nil,
+			},
+			update: method[any]{
+				err: nil,
+			},
 		},
 		{
-			name:             "ErrAddPhotos",
-			addCheckWantErr:  false,
-			addPhotosWantErr: true,
+			name: "ErrAddPhotos",
+			addCheck: method[int64]{
+				data: int64(1),
+				err:  nil,
+			},
+			addPhotos: method[any]{
+				err: errors.New(""),
+			},
+			update: method[any]{
+				err: nil,
+			},
+		},
+		{
+			name: "ErrUpdate",
+			addCheck: method[int64]{
+				data: int64(1),
+				err:  nil,
+			},
+			addPhotos: method[any]{
+				err: nil,
+			},
+			update: method[any]{
+				err: errors.New(""),
+			},
 		},
 	}
 	for _, tt := range tests {
 		suite.Run(tt.name, func() {
-			checksRepoCall := suite.checksRepo.On("AddCheck", mock.Anything, mock.Anything).Once()
-			if !tt.addCheckWantErr {
-				checksRepoCall.Return(int64(1), nil)
-
-				photosRepoCall := suite.photosRepo.On("AddPhotos", mock.Anything, mock.AnythingOfType("int"), mock.AnythingOfType("int"), mock.Anything).Once()
-				if !tt.addPhotosWantErr {
-					photosRepoCall.Return(nil)
-				} else {
-					photosRepoCall.Return(errors.New(""))
+			func() {
+				suite.checksRepo.On("AddCheck", mock.Anything, mock.Anything).Once().
+					Return(tt.addCheck.data, tt.addCheck.err)
+				if tt.addCheck.err != nil {
+					return
 				}
-			} else {
-				checksRepoCall.Return(int64(0), errors.New(""))
-			}
+
+				suite.photosRepo.On("AddPhotos", mock.Anything, mock.AnythingOfType("int"), mock.AnythingOfType("int"), mock.Anything).Once().
+					Return(tt.addPhotos.err)
+				if tt.addPhotos.err != nil {
+					return
+				}
+
+				suite.updater.On("Update", mock.Anything, mock.AnythingOfType("int")).Once().
+					Return(tt.update.err)
+				if tt.update.err != nil {
+					return
+				}
+			}()
 
 			_, gotErr := suite.uc.AddCheck(context.Background(), models.Check{}, []io.Reader{})
 
-			if !tt.addCheckWantErr && !tt.addPhotosWantErr {
+			if tt.addCheck.err == nil && tt.addPhotos.err == nil && tt.update.err == nil {
 				suite.NoError(gotErr)
 			} else {
 				suite.NotNil(gotErr)
@@ -204,6 +259,234 @@ func (suite *ChecksSuite) TestGetChecksByUserId() {
 			_, gotErr := suite.uc.GetChecksByUserId(context.Background(), 1)
 
 			if tt.errGetChecksByUserId == nil && tt.errGetPhotosByCheckId == nil {
+				suite.NoError(gotErr)
+			} else {
+				suite.NotNil(gotErr)
+			}
+		})
+	}
+}
+
+type MarkStatusUpdaterSuite struct {
+	suite.Suite
+	u          *usecase.Updater
+	log        *slog.Logger
+	marksRepo  *usecase.MockMarksRepository
+	checksRepo *usecase.MockChecksRepository
+}
+
+func (suite *MarkStatusUpdaterSuite) SetupSuite() {
+	suite.log = slogdiscard.NewDiscardLogger()
+	suite.marksRepo = usecase.NewMockMarksRepository(suite.T())
+	suite.checksRepo = usecase.NewMockChecksRepository(suite.T())
+	suite.u = usecase.NewUpdater(suite.log, usecase.UpdaterRepositories{
+		Marks:  suite.marksRepo,
+		Checks: suite.checksRepo,
+	})
+}
+
+func TestMarkStatusUpdater(t *testing.T) {
+	suite.Run(t, new(MarkStatusUpdaterSuite))
+}
+
+func (suite *MarkStatusUpdaterSuite) TestUpdateMarkStatus() {
+	type method[T any] struct {
+		data T
+		err  error
+	}
+
+	tests := []struct {
+		name              string
+		getMarkById       method[models.Mark]
+		getChecksByMarkId method[[]models.Check]
+		wantUpdated       bool
+		updateMarkStatus  method[any]
+	}{
+		{
+			name: "Ok",
+			getMarkById: method[models.Mark]{
+				data: models.Mark{
+					MarkStatusID: int(models.UnconfirmedStatus),
+				},
+				err: nil,
+			},
+			getChecksByMarkId: method[[]models.Check]{
+				data: []models.Check{
+					{
+						Result: true,
+					},
+					{
+						Result: false,
+					},
+				},
+				err: nil,
+			},
+		},
+		{
+			name: "Ok-ConfirmedStatus",
+			getMarkById: method[models.Mark]{
+				data: models.Mark{
+					MarkStatusID: int(models.UnconfirmedStatus),
+				},
+				err: nil,
+			},
+			getChecksByMarkId: method[[]models.Check]{
+				data: []models.Check{
+					{
+						Result: true,
+					},
+					{
+						Result: true,
+					},
+					{
+						Result: true,
+					},
+				},
+				err: nil,
+			},
+			wantUpdated: true,
+			updateMarkStatus: method[any]{
+				err: nil,
+			},
+		},
+		{
+			name: "Err-ConfirmedStatus",
+			getMarkById: method[models.Mark]{
+				data: models.Mark{
+					MarkStatusID: int(models.UnconfirmedStatus),
+				},
+				err: nil,
+			},
+			getChecksByMarkId: method[[]models.Check]{
+				data: []models.Check{
+					{
+						Result: true,
+					},
+					{
+						Result: true,
+					},
+					{
+						Result: true,
+					},
+				},
+				err: nil,
+			},
+			wantUpdated: true,
+			updateMarkStatus: method[any]{
+				err: errors.New(""),
+			},
+		},
+		{
+			name: "Ok-RefutedStatus",
+			getMarkById: method[models.Mark]{
+				data: models.Mark{
+					MarkStatusID: int(models.UnconfirmedStatus),
+				},
+				err: nil,
+			},
+			getChecksByMarkId: method[[]models.Check]{
+				data: []models.Check{
+					{
+						Result: false,
+					},
+					{
+						Result: false,
+					},
+					{
+						Result: false,
+					},
+				},
+				err: nil,
+			},
+			wantUpdated: true,
+			updateMarkStatus: method[any]{
+				err: nil,
+			},
+		},
+		{
+			name: "Ok-RefutedStatus",
+			getMarkById: method[models.Mark]{
+				data: models.Mark{
+					MarkStatusID: int(models.UnconfirmedStatus),
+				},
+				err: nil,
+			},
+			getChecksByMarkId: method[[]models.Check]{
+				data: []models.Check{
+					{
+						Result: false,
+					},
+					{
+						Result: false,
+					},
+					{
+						Result: false,
+					},
+				},
+				err: nil,
+			},
+			wantUpdated: true,
+			updateMarkStatus: method[any]{
+				err: errors.New(""),
+			},
+		},
+		{
+			name: "Err-GetMarkById",
+			getMarkById: method[models.Mark]{
+				data: models.Mark{},
+				err:  errors.New(""),
+			},
+		},
+		{
+			name: "Err-GetChecksByMarkId",
+			getMarkById: method[models.Mark]{
+				data: models.Mark{
+					MarkStatusID: int(models.UnconfirmedStatus),
+				},
+				err: nil,
+			},
+			getChecksByMarkId: method[[]models.Check]{
+				data: []models.Check{
+					{
+						Result: true,
+					},
+					{
+						Result: false,
+					},
+				},
+				err: errors.New(""),
+			},
+		},
+	}
+	for _, tt := range tests {
+		suite.Run(tt.name, func() {
+			func() {
+				suite.marksRepo.On("GetMarkById", mock.Anything, mock.AnythingOfType("int")).Once().
+					Return(tt.getMarkById.data, tt.getMarkById.err)
+				if tt.getMarkById.err != nil {
+					return
+				}
+
+				if tt.getMarkById.data.MarkStatusID == int(models.UnconfirmedStatus) {
+					suite.checksRepo.On("GetChecksByMarkId", mock.Anything, mock.AnythingOfType("int")).Once().
+						Return(tt.getChecksByMarkId.data, tt.getChecksByMarkId.err)
+					if tt.getChecksByMarkId.err != nil {
+						return
+					}
+
+					if tt.wantUpdated {
+						suite.marksRepo.On("UpdateMarkStatus", mock.Anything, mock.AnythingOfType("int"), mock.Anything).Once().
+							Return(tt.updateMarkStatus.err)
+						if tt.updateMarkStatus.err != nil {
+							return
+						}
+					}
+				}
+			}()
+
+			gotErr := suite.u.Update(context.Background(), 1)
+
+			if tt.getMarkById.err == nil && tt.getChecksByMarkId.err == nil && tt.updateMarkStatus.err == nil {
 				suite.NoError(gotErr)
 			} else {
 				suite.NotNil(gotErr)
