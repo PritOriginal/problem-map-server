b335f7fb8631511362130f59eb6a3d977a56d2fb
Махнатеев Степан <stepan01062004@mail.ru.>
2026-02-27 18:09:11 +0300

    New migration to add logging when creating a marker

---
+++ migrations/000017_add_logging_when_creating_a_marker.up.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION log_mark_status_change()
+RETURNS TRIGGER AS $$ 
+BEGIN
+    INSERT INTO mark_status_history (mark_id, old_mark_status_id, new_mark_status_id)
+    VALUES (NEW.mark_id, OLD.mark_status_id, NEW.mark_status_id);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER insert_mark_status 
+AFTER INSERT ON marks 
+FOR EACH ROW 
+EXECUTE FUNCTION log_mark_status_change();
