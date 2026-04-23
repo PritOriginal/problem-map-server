22e4c1a57e78be1b0536fdd98c988cc4eb1e6cfa
Махнатеев Степан <stepan01062004@mail.ru>
2026-04-23 20:48:18 +0300

    test: add functional tests for admin boundaries endpoints

--- tests/rest/map_test.go
+++ tests/rest/map_test.go
@@ -27,6 +27,128 @@ func TestMapSuite(t *testing.T) {
 	suite.Run(t, new(MapSuite))
 }
 
+func (st *MapSuite) TestGetAdminBoundaries() {
+	tests := []struct {
+		name       string
+		query      string
+		statusCode int
+	}{
+		{
+			name:       "Ok200",
+			query:      "",
+			statusCode: http.StatusOK,
+		},
+		{
+			name:       "Ok200",
+			query:      "?admin_levels=9",
+			statusCode: http.StatusOK,
+		},
+		{
+			name:       "Ok200",
+			query:      "?admin_levels=9&admin_levels=10",
+			statusCode: http.StatusOK,
+		},
+		{
+			name:       "Err400",
+			query:      "?admin_levels=a",
+			statusCode: http.StatusBadRequest,
+		},
+		{
+			name:       "Err400",
+			query:      "?admin_levels=9,10",
+			statusCode: http.StatusBadRequest,
+		},
+	}
+
+	for _, tt := range tests {
+		st.Run(tt.name, func() {
+			resp, err := http.Get(
+				fmt.Sprintf("http://%s:%d/map/admin-boundaries%s",
+					st.Cfg.REST.Host,
+					st.Cfg.REST.Port,
+					tt.query,
+				),
+			)
+			st.NoError(err)
+			defer resp.Body.Close()
+
+			st.Equal(tt.statusCode, resp.StatusCode)
+
+			var response responses.Response[maprest.GetAdminBoundariesResponse]
+			err = json.NewDecoder(resp.Body).Decode(&response)
+			st.NoError(err)
+
+			if tt.statusCode < 300 {
+				st.Equal(response.Success, true)
+				st.NotNil(response.Payload.AdminBoundaries)
+			} else {
+				st.Equal(response.Success, false)
+			}
+		})
+	}
+}
+
+func (st *MapSuite) TestGetAdminBoundariesMarksCount() {
+	tests := []struct {
+		name       string
+		query      string
+		statusCode int
+	}{
+		{
+			name:       "Ok200",
+			query:      "",
+			statusCode: http.StatusOK,
+		},
+		{
+			name:       "Ok200",
+			query:      "?admin_levels=9",
+			statusCode: http.StatusOK,
+		},
+		{
+			name:       "Ok200",
+			query:      "?admin_levels=9&admin_levels=10",
+			statusCode: http.StatusOK,
+		},
+		{
+			name:       "Err400",
+			query:      "?admin_levels=a",
+			statusCode: http.StatusBadRequest,
+		},
+		{
+			name:       "Err400",
+			query:      "?admin_levels=9,10",
+			statusCode: http.StatusBadRequest,
+		},
+	}
+
+	for _, tt := range tests {
+		st.Run(tt.name, func() {
+			resp, err := http.Get(
+				fmt.Sprintf("http://%s:%d/map/admin-boundaries/marks/count%s",
+					st.Cfg.REST.Host,
+					st.Cfg.REST.Port,
+					tt.query,
+				),
+			)
+			st.NoError(err)
+			defer resp.Body.Close()
+
+			st.Equal(tt.statusCode, resp.StatusCode)
+
+			var response responses.Response[maprest.GetAdminBoundariesMarksCountResponse]
+			err = json.NewDecoder(resp.Body).Decode(&response)
+			st.NoError(err)
+
+			if tt.statusCode < 300 {
+				st.Equal(response.Success, true)
+				st.NotNil(response.Payload.AdminBoundaries)
+			} else {
+				st.Equal(response.Success, false)
+			}
+		})
+	}
+}
+
 func (st *MapSuite) TestGetRegions() {
 	resp, err := http.Get(fmt.Sprintf("http://%s:%d/map/regions", st.Cfg.REST.Host, st.Cfg.REST.Port))
 
