86a95ee80605badc9469df0bf8c3d47f98e2c081
Махнатеев Степан <stepan01062004@mail.ru>
2026-05-01 19:03:14 +0300

    test: add make url func for functional tests

--- tests/rest/suite_test.go
+++ tests/rest/suite_test.go
@@ -1,6 +1,8 @@
 package tests
 
 import (
+	"fmt"
+	"net/url"
 	"testing"
 
 	"github.com/PritOriginal/problem-map-server/internal/config"
@@ -28,4 +30,26 @@ func (st *Suite) SetupSuite() {
 
 func Test(t *testing.T) {
 	suite.Run(t, new(Suite))
+}
+
+type makeUrlParams struct {
+	host  string
+	port  int
+	path  string
+	query string
+}
+
+func makeUrl(params makeUrlParams) string {
+	host := params.host
+	if params.port > 0 {
+		host = fmt.Sprintf("%s:%d", params.host, params.port)
+	}
+
+	u := url.URL{
+		Scheme:   "http",
+		Host:     host,
+		Path:     params.path,
+		RawQuery: params.query,
+	}
+	return u.String()
 }
