dbfbbad494d562a004e82321fcef0918aa9eebf6
Махнатеев Степан <stepan01062004@mail.ru>
2026-08-29 14:50:47 +0300

    perf(db): 000034 composite sort indexes for marks, tasks and checks listings

---
+++ migrations/000034_add_sort_indexes.down.sql
@@ -0,0 +1,5 @@
+DROP INDEX IF EXISTS idx_checks_mark_id_created_at_desc;
+DROP INDEX IF EXISTS idx_tasks_user_id_created_at_desc;
+DROP INDEX IF EXISTS idx_marks_status_created_at_desc;
+DROP INDEX IF EXISTS idx_marks_updated_at_desc;
+DROP INDEX IF EXISTS idx_marks_created_at_desc;
